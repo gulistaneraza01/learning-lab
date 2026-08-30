ec0c63f8c6cfe8c9ca617fd1bc29c2cf8e8e3ec3
Gulistane Raza <gulistaneraza01@gmail.com>
2026-08-31 01:43:32 +0530

    Add new examples for slice manipulation in main.go, including functions for printing capacity and length, and additional examples for array and matrix handling.

--- go/Day06/slice/main.go
+++ go/Day06/slice/main.go
@@ -10,13 +10,77 @@ func main() {
 	// example1()
 	example2()
 	// example3()
+	// example4()
+	// example5()
+	// example6()
+
+}
+
+func example6() {
+	num1 := make([]int, 0, 10)
+	if num1 == nil {
+		fmt.Print("---> ni;l")
+	}
+	for i, v := range num1 {
+		fmt.Printf("num1[%d]: %d\n", i, v)
+	}
+
+	// printCapInt(num1)
+
+	matrix := [][]int{
+		[]int{1, 2, 3},
+		[]int{1, 2, 3},
+	}
+
+	fmt.Println(matrix)
+}
+
+func example5() {
+	var arr [2]string
+	// if arr == nil {
+	// 	fmt.Println("nill")
+	// }
+
+	for i, v := range arr {
+		fmt.Printf("arr[%d]: '%s'\n", i, v)
+	}
+}
+
+func example4() {
+	num1 := []string{"a", "b", "c", "d", "e"}
+	printCap(num1)
+
+	num1 = num1[1:4]
+	printCap(num1)
+
+	num1 = num1[0:2]
+	printCap(num1)
+
+	fmt.Println(num1[0])
+	num1[1] = "z"
+	printCap(num1)
+
+	// num1 = num1[0]
+	// printCap(num1)
+
+}
+
+func printCap(a []string) {
+	fmt.Println("cap:", cap(a), "len:", len(a), "-->", a)
+}
+
+func printCapInt(a []int) {
+	fmt.Println("cap:", cap(a), "len:", len(a), "-->", a)
 }
 
 func example2() {
 
 	num1 := []int{1, 2, 3, 4, 5}
 	num2 := append(num1, 3)
-	// num2[3] = 10
+	num1[2] = 10
+	fmt.Println(num1, num2)
+
+	fmt.Println("----->", cap(num1), cap(num2))
 
 	strat := num1[1:]
 	end := num1[:2]
