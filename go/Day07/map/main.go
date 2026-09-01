10b8ed19de9307e8b5ef1bae6edee52433f74a46
Gulistane Raza <gulistaneraza01@gmail.com>
2026-09-02 01:01:03 +0530

    Add example for map manipulation in main.go, including deletion of keys, adding new entries, and iterating over the map contents.

--- go/Day07/map/main.go
+++ go/Day07/map/main.go
@@ -16,8 +16,21 @@ func example2() {
 	m["one"] = 1
 	m["two"] = 2
 	fmt.Println(m)
+
 	m["two"] = 3
 	fmt.Println(m)
+
+	delete(m, "three")
+	m["five"] = 0
+	v, ok := m["five"]
+	fmt.Println(m, v, ok)
+
+	// fmt.Println("value-->", m["four"])
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
 }
 
 type Person struct {
