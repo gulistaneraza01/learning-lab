e32f9ee0f4ea807bca191fcb8377746b66826edc
Gulistane Raza <gulistaneraza01@gmail.com>
2026-09-12 21:00:21 +0530

    Add concurrent summation example in main.go, implementing a new function to calculate totals from slices using goroutines and channels.

--- go/Day08/concurrence/main.go
+++ go/Day08/concurrence/main.go
@@ -7,9 +7,35 @@ import (
 
 func main() {
 	// fmt.Println("hello raza")
-	go example1("hello")
+	// example1("hello")
 
-	example1("raza")
+	// example1("raza")
+	examples2()
+}
+
+func examples2() {
+	nums := []int{6, 2, 4, 6}
+
+	ch := make(chan int)
+
+	go total(nums[:len(nums)/2], ch)
+
+	go total(nums[len(nums)/2:], ch)
+
+	a := <-ch
+	b := <-ch
+	fmt.Println(a, b)
+
+}
+
+func total(nums []int, ch chan int) {
+	result := 0
+
+	for _, num := range nums {
+		result += num
+	}
+
+	ch <- result
 }
 
 func example1(str string) {
