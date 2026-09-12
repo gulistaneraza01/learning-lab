2951b09701ed82957e427dd61e80382e5030fdb2
Gulistane Raza <gulistaneraza01@gmail.com>
2026-09-12 16:27:14 +0530

    Implement concurrent execution in main.go by adding a new example function that prints a string multiple times with a delay, demonstrating goroutine usage.

--- go/Day08/concurrence/main.go
+++ go/Day08/concurrence/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello raza")
+	// fmt.Println("hello raza")
+	go example1("hello")
+
+	example1("raza")
+}
+
+func example1(str string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(str)
+	}
+
 }
