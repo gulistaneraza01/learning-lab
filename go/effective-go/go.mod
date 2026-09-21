2b918b7361715b5f663cb43110378a4b5b2e4951
Gulistane Raza <gulistaneraza01@gmail.com>
2026-09-21 22:41:47 +0530

    Add new Go module and initial implementation of main.go with a greeting message and comments on Go best practices.

---
+++ go/effective-go/go.mod
@@ -0,0 +1,3 @@
+module github.com/gulistaneraza01/effective-go
+
+go 1.26.4
---
+++ go/effective-go/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello Raza")
+
+	// go have default fromatter
+	// for export use Uppercase of first leter
+	// use package name maningful
+	// interface name have sufix with -er
+	// identifier name are follow Camalcase
+	// same package i can err var multiple places
+
+}
